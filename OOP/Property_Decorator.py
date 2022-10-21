5ec5a1886132b013195ef6ecdde0768e9fa7747d
Dr. Saad Laouadi <data16scientist@gmail.com>
2022-10-21 22:31:49 +0300

    Update Property_Decorator.py

--- OOP/Property_Decorator.py
+++ OOP/Property_Decorator.py
@@ -1,21 +1,55 @@
 # Python Code: Review Classes' Concepts
 class Holding:
     """Description of the class ."""
-    # You have to stick the same style. 
+    # You have to stick the same style.
 
-    def __init__(self, name, date, nshares, price):
-        self._name = name 
-        self._date = date 
-        self._nshares = nshares 
-        self._price = price 
-        
+    def __init__(self, name, date, shares, price):
+        self._name = name
+        self._date = date
+        self._shares = shares
+        self._price = price
+
     def __repr__(self):
-        return f"<{self._name} {self._date} {self._nshares} {self._price}>"
+        return f"<{self._name} {self._date} {self._shares} {self._price}>"
 
 
     def __str__(self):
-        return (f"The shares' names {self._name} " +
-                    f"The date: {self._date} "+
-                    f"The number of shares is: {self._nshares} "+
+        return (f"The shares' names {self._name}, " +
+                    f"The date: {self._date}, "+
+                    f"The number of shares is: {self._shares}, "+
                     f"The price of each share is: {self._price}")
-    
+
+
+    def sell_shares(self, nshares):
+        self._shares = self._shares - nshares
+
+    # Create a buy_shares method.
+    def buy_shares(self, nshares):
+        self._shares += nshares
+
+
+    def total_return(self):
+        return self._shares * self._price
+from holding import Holding
+
+
+h1 = Holding("IBM", "2020-1-10", 50, 47.3)
+
+# print(h1.__dict__)
+#
+# # print(h1)
+# print(repr(h1))
+
+# Print the number of shares
+# print(h1._shares)
+#
+# # Apply the sell method
+# h1.sell_shares(20)
+# print(h1._shares)
+#
+# # Apply the buy_method
+# h1.buy_shares(65)
+
+print(h1._shares)
+
+print(h1.total_return())
