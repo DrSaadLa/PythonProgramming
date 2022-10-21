e7d68ae1a19dd68b9322f3c8e8466f8168c28e01
Dr. Saad Laouadi <data16scientist@gmail.com>
2022-10-21 21:52:36 +0300

    Update Property_Decorator.py

--- OOP/Property_Decorator.py
+++ OOP/Property_Decorator.py
@@ -1,1 +1,18 @@
 # Python Code: Review Classes' Concepts
+class Holding:
+    """Description of the class ."""
+    # You have to stick the same style. 
+
+    def __init__(self, name, date, nshares, price):
+        self._name = name 
+        self._date = date 
+        self._nshares = nshares 
+        self._price = price 
+
+    def __repr__():
+        pass # repr()
+
+
+    def __str__(self):
+        pass # print()
+    
