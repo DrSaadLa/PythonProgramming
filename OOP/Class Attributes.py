8682047b78c5e23853885d2d831bfbab6386e77f
Dr. Saad Laouadi <data16scientist@gmail.com>
2022-11-04 22:04:43 +0300

    Update Class Attributes.py

--- OOP/Class Attributes.py
+++ OOP/Class Attributes.py
@@ -1,1 +1,10 @@
 # Class attribues
+class Employee:
+    def __init__(self, name, salary):
+        self.name = name
+        # self.salary = salary
+        if salary < 1000:
+            raise ValueError(f"The salary cannot be less than {1000}" \
+            " Please provide a reasonable salary")
+        if salary >= 1000:
+             self.salary = salary
