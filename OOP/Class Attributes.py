b1fa5def3d1db73cde1aa91b764761950ea8bd65
Dr. Saad Laouadi <data16scientist@gmail.com>
2022-11-04 22:31:10 +0300

    Update Class Attributes.py

--- OOP/Class Attributes.py
+++ OOP/Class Attributes.py
@@ -8,3 +8,17 @@ class Employee:
             " Please provide a reasonable salary")
         if salary >= 1000:
              self.salary = salary
+    def give_raise(self, raise_pct):
+        self.salary = int(self.salary * raise_pct)
+                
+# Practice 
+
+class Employee:
+    BASE_SALARY = 1000 # <---- This is called Class Attribute
+    def __init__(self, name, salary):
+        self.name = name      # <--- Instance attribute
+
+        if salary >= Employee.BASE_SALARY:
+            self.salary = salary
+        else:
+            self.salary = Employee.BASE_SALARY
