45126401f333e33099c2e0ea4e2cc18aba2f55be
Dr. Saad Laouadi <data16scientist@gmail.com>
2022-11-11 22:00:09 +0300

    Update properties.py

--- OOP/properties.py
+++ OOP/properties.py
@@ -10,3 +10,38 @@ class Professor:
 
     def fullname(self):
         return f"{self.firstname} {self.lastname}"
+    
+    
+class Professor:
+    """Professor class."""
+
+    def __init__(self, firstname, lastname, title):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.title = title
+    
+    def fullname(self):
+        return f"{self.firstname} {self.lastname}"
+    
+    def email(self):
+        return (f"{self.title}{self.firstname.lower()}"+
+        f"{'_'}{self.lastname.lower()}{'@school.edu'}")
+    
+    
+    prof_1 = Professor("Saad", "Laouadi", "Dr.")
+prof_2 = Professor("Nassim", "Ali", "Pro.")
+prof_3 = Professor("Muhammad", "Nadir", "Ass.")
+
+print("*"*40)
+print(prof_1.email())      # This is called referencing a function 
+print(prof_2.email())
+print(prof_3.email())
+
+# To change 
+prof_1.title = "Pr."
+prof_3.title = "Dr."
+
+print("*"*40)
+print(prof_1.email())
+print(prof_2.email())
+print(prof_3.email())
